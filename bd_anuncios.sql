c522f3fc4a0d8667528d920ce0d81fa36fb70dfc
Az-bits <09alanocaramirez2000@gmail.com>
2023-05-23 13:13:02 -0400

    version 1

--- bd_anuncios.sql
+++ bd_anuncios.sql
@@ -6,7 +6,7 @@
 -- Tiempo de generación: 23-05-2023 a las 18:42:12
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
-
+--#comentario
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
